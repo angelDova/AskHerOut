41fcd0abba722c6033291f2861d000e8455d7e93
angelDova <Angelfcb01@yahoo.com>
2024-01-05 19:33:28 -0800

    6: framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import Confetti from "react-confetti";
 import { useConfettiStore } from "@/hooks/use-confetti-store";
 import { toast } from "sonner";
+import { Variant, motion, useAnimation, useInView } from "framer-motion";
 
-export default function Home() {
+export const Home = () => {
   const router = useRouter();
   const confetti = useConfettiStore();
   const [isLoading, setIsLoading] = useState(false);
@@ -86,9 +86,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center h-[100vh] bg-[#A2E5E8] lg:p-20 p-12">
       <div className="mb-16">
-        <h1 className="text-5xl font-bold text-white text-center mt-[10px] mb-0">
-          Will you be my girlfriend?
-        </h1>
+        <motion.h1 className="text-5xl font-bold text-white text-center mt-[10px] mb-0">
+          <AnimatedText
+            text={["Will you be my girlfriend?"]}
+            repeatDelay={10000}
+          />
+        </motion.h1>
+
         <div className="flex items-center justify-center">
           <Image
             src={"/images/elly-belly1.gif"}
@@ -124,4 +128,102 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+type AnimatedTextProps = {
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  className?: string;
+  once?: boolean;
+  repeatDelay?: number;
+  animation?: {
+    hidden: Variant;
+    visible: Variant;
+  };
+};
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+export const AnimatedText = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}: AnimatedTextProps) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView, controls, repeatDelay]);
+
+  return (
+    <Wrapper className={className}>
+      <span className="sr-only">{textArray.join(" ")}</span>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={`${word}-${wordIndex}`}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
+  );
+};
+
+export default Home;
